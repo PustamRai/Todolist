51a5e3ed1e3965698c53f055217ea1c7eecafaa4
Pustam Rai <github.pustam@gmail.com>
2025-01-17 18:46:56 +0545

    implement task update functionality

--- backend/src/controllers/todo.controller.js
+++ backend/src/controllers/todo.controller.js
@@ -3,29 +3,75 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 
 const addTask = async (req, res) => {
-        const  { text } = req.body
+        try {
+            const  { text } = req.body
+        
+            if(!text || typeof text !== "string" || text.trim() === "") {
+                throw new ApiError(400, "the 'text' field is required and must not be empty")
+            }
+        
+            const newTask = await Task.create({
+                text: text
+            })
+        
+            if(!newTask) {
+                throw new ApiError(400, "Failed to add task to the database")
+            }
+        
+            return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    newTask,
+                    "New task successfully added"
+                )
+            );
+        } catch (error) {
+            throw new ApiError(400, "Error adding task: ", error)
+        }
+}
+
+const updateTask = async (req, res) => {
+    try {
+        const { _id, text } = req.body
     
-        if(!text || typeof text !== "string" || text.trim() === "") {
-            throw new ApiError(400, "the 'text' field is required and must not be empty")
+        if(!_id && !text) {
+            throw new ApiError(400, "id and text field is required")
         }
     
-        const newTask = await Task.create({
-            text: text
-        })
-    
-        if(!newTask) {
-            throw new ApiError(400, "Failed to add task to the database")
+        const updatedTask = await Task.findByIdAndUpdate(
+            _id,
+            {
+                $set: {
+                    text
+                }
+            },
+            {
+                new: true // return the updated document
+            }
+        )
+
+        if (!updatedTask) {
+            throw new ApiError(404, "Task not found");
         }
-    
+        
         return res
         .status(200)
         .json(
             new ApiResponse(
                 200,
-                newTask,
-                "New task successfully added"
+                updatedTask,
+                "task updated successfully"
             )
-        );
+        )
+    } catch (error) {
+        throw new ApiError(400, "Error updating task: ", error)
+    }
+
 }
 
-export { addTask }
+export { 
+    addTask,
+    updateTask
+}
--- backend/src/routes/todo.route.js
+++ backend/src/routes/todo.route.js
@@ -1,8 +1,12 @@
 import { Router } from "express"
-import { addTask } from "../controllers/todo.controller.js";
+import { 
+    addTask,
+    updateTask
+} from "../controllers/todo.controller.js";
 
 const router = Router();
 
 router.route("/addTask").post(addTask)
+router.route("/updateTask").post(updateTask)
 
 export default router
