1c4c0c9f69c78a715432734e086a858254a4865c
Pustam Rai <github.pustam@gmail.com>
2025-02-24 22:01:31 +0545

    fix vercel deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import dotenv, { configDotenv } from "dotenv"
 import { app } from "./app.js"
 import connectDB from "./db/db.js"
+import { app } from "./app.js";
 
 configDotenv();
 
@@ -8,11 +9,13 @@ const PORT = process.env.PORT || 8000;
 
 // when connection is build with "async" with database it returns promises automatically. So, .then and .catch is required to handle the error.
 connectDB()
-.then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server is running at PORT: ${PORT}`);
-    })
-})
-.catch((err) => {
-    console.error("Mongodb connection failed!!", err)
-})
+// .then(() => {
+//     app.listen(PORT, () => {
+//         console.log(`Server is running at PORT: ${PORT}`);
+//     })
+// })
+// .catch((err) => {
+//     console.error("Mongodb connection failed!!", err)
+// })
+
+export default app
