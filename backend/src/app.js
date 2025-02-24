c5bc94fbd6dcfac9df064096028a7b1a6a960ee0
Pustam Rai <github.pustam@gmail.com>
2025-02-24 16:45:41 +0545

    update cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,7 @@ import cors from "cors"
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }))
